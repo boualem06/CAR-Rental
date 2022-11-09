0f05f0261b5cd2b9d67e6c74e80a1b28c279718b
boualem06 <kb_hamroune@esi.dz>
2022-11-10 00:12:09 +0100

    solving the problem in details cars page

--- Front End/src/Components/CarsDetails.js
+++ Front End/src/Components/CarsDetails.js
@@ -8,7 +8,7 @@ const CartDetails = () => {
     const [principaleImage, setPrincipaleImage] = useState(car1);
     return (
         <div style={{ backgroundColor: "#f3f4f8" }} className="w-full flex lg:flex-row flex-col px-4 py-4">
-            <div className="  px-4 py-2  rounded-lg mb-4 lg:mr-4 lg:mb-0">
+            <div className="  px-4 py-2  rounded-lg mb-4 lg:mr-4 lg:mb-0 ">
                 <div className='bg-white rounded-lg'>
                     <img src={principaleImage} alt="car" ></img>
                 </div>
@@ -17,14 +17,15 @@ const CartDetails = () => {
                     <button onClick={() => { setClicked(2); setPrincipaleImage(car2) }} style={{ borderColor: ` ${clicked === 2 ? "#546fff" : "#f3f4f8"}` }} className='border rounded-lg border-2 py-4 bg-white' ><img src={car2} alt="Car" className='max-w-full h-auto '></img></button>
                     <button onClick={() => { setClicked(3) }} style={{ borderColor: ` ${clicked === 3 ? "#546fff" : "#f3f4f8"}` }} className='border rounded-lg border-2 py-4 bg-white'><img src={car1} alt="Car" className='max-w-full h-auto '></img></button>
                 </div>
+                {/* <div className=''>Test1</div> */}
             </div>
-            <div className="  px-4 py-2  rounded-lg   grid grid-rows-4 bg-white ">
-                <div>
+            <div className="  px-4  py-4 rounded-lg  lg:grid lg:grid-rows-4 bg-white ">
+                <div className='mb-8 lg:mb-0'>
                     <h1 className='text-2xl font-bold'>Porch Boxer </h1>
                     <Rating name="read-only" value={3} readOnly />
                 </div>
-                <p className=''>The Porsche Boxster is a mid-engine two-seater roadster. It was Porsche's first road vehicle to be originally designed as a roadster since the 914. The first-generation Boxster (the 986) was introduced in late 1996; it was powered by a 2.5-litre flat six-cylinder engine.</p>
-                <div className='grid grid-cols-4'>
+                <p className='mb-8 lg:mb-0'>The Porsche Boxster is a mid-engine two-seater roadster. It was Porsche's first road vehicle to be originally designed as a roadster since the 914. The first-generation Boxster (the 986) was introduced in late 1996; it was powered by a 2.5-litre flat six-cylinder engine.</p>
+                <div className='grid grid-cols-4 mb-8 lg:mb-0'>
                     <div>
                         <h1 className='text-gray-400'>Type</h1>
                         <h1 className='text-gray-400'>Steering</h1>
@@ -48,6 +49,7 @@ const CartDetails = () => {
                     <button style={{ backgroundColor: "#546fff" }} className="font-bold px-6 py-2 text-white rounded-md mt-2 ">Rent Now</button>
 
                 </div>
+                {/* <div>Test2</div> */}
             </div>
         </div>
     )
